7ffd208142b89f25421d0c3291a2bbdaeb4c35bd
Aashritha <anashrita99@gmail.com>
2024-11-18 20:30:05 +0530

    modified the header,Added a shadow element

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+//header scroll
+let header = document.querySelector('header');
+
+window.addEventListener('scroll',()=> {
+header.classList.toggle('shadow',window.scrollY>0);
+});
+
 //swiper
 var swiper = new Swiper(".mySwiper ", {
     spaceBetween: 30,
